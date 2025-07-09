867c75916bd9bcff7015a877839159644dc26de4
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-07-09 17:26:05 +0700

    Menambahkan struct detailAlamat dengan atribut desa dan kota

---
+++ struct2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+}
